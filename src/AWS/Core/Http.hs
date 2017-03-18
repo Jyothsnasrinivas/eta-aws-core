a491571a3af95d8524e3a963458f314ad7c51572
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-18 23:24:55 +0530

    Started AWS.Core.Http

---
+++ src/AWS/Core/Http.hs
@@ -0,0 +1,34 @@
+module AWS.Core.Http where
+
+import Java
+import Java.IO
+import Java.Net
+import AWS.Core.Types
+
+-- Start com.amazonaws.http.HttpMethodName
+
+data {-# CLASS "com.amazonaws.http.HttpMethodName" #-} HttpMethodName = HttpMethodName (Object# HttpMethodName)
+  deriving Class
+
+foreign import java unsafe "@static @field com.amazonaws.http.HttpMethodName.DELETE"
+  hmnDELETE :: HttpMethodName
+
+foreign import java unsafe "@static @field com.amazonaws.http.HttpMethodName.GET"
+  hmnGET :: HttpMethodName
+
+foreign import java unsafe "@static @field com.amazonaws.http.HttpMethodName.HEAD"
+  hmnHEAD :: HttpMethodName
+
+foreign import java unsafe "@static @field com.amazonaws.http.HttpMethodName.OPTIONS"
+  hmnOPTIONS :: HttpMethodName
+
+foreign import java unsafe "@static @field com.amazonaws.http.HttpMethodName.PATCH"
+  hmnPATCH :: HttpMethodName
+
+foreign import java unsafe "@static @field com.amazonaws.http.HttpMethodName.POST"
+  hmnPOST :: HttpMethodName
+
+foreign import java unsafe "@static @field com.amazonaws.http.HttpMethodName.PUT"
+  hmnPUT :: HttpMethodName
+
+-- End com.amazonaws.http.HttpMethodName
