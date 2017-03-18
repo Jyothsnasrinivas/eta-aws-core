70f72e8a1ecee9c947535090f81da563b671c0d7
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-19 01:09:37 +0530

    Added RequestHandler to AWS.Core.Handlers

--- src/AWS/Core/Handlers.hs
+++ src/AWS/Core/Handlers.hs
@@ -11,3 +11,26 @@ data {-# CLASS "com.amazonaws.handlers.HandlerContextKey" #-} HandlerContextKey 
   deriving Class
 
 -- End com.amazonaws.handlers.HandlerContextKey
+
+-- Start com.amazonaws.handlers.RequestHandler
+
+data {-# CLASS "com.amazonaws.handlers.RequestHandler" #-} RequestHandler = RequestHandler (Object# RequestHandler)
+  deriving Class
+
+foreign import java unsafe "@interface afterError"
+  afterError :: Request b -> Exection -> Java RequestHandler ()
+
+foreign import java unsafe "@interface afterResponse"
+  afterResponse :: Request b -> Object -> TimingInfo -> Java RequestHandler ()
+
+foreign import java unsafe "@interface beforeRequest"
+  beforeRequest :: Request b -> Java RequestHandler ()
+
+-- End com.amazonaws.handlers.RequestHandler
+
+-- Start com.amazonaws.handlers.RequestHandler2
+
+data {-# CLASS "com.amazonaws.handlers.RequestHandler2" #-} RequestHandler2 = RequestHandler2 (Object# RequestHandler2)
+  deriving Class
+
+-- End com.amazonaws.handlers.RequestHandler2
