b7a2f9d6ef0c3e9b822de225a9afa2e29f8bb6eb
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-18 21:53:50 +0530

    Added Request to Types

--- src/AWS/Core/Types.hs
+++ src/AWS/Core/Types.hs
@@ -90,9 +90,9 @@ foreign import java unsafe "@static @field com.amazonaws.http.HttpMethodName.PUT
 
 -- End com.amazonaws.http.HttpMethodName
 
--- Start com.amazonaws.http.HttpMethodName
+-- Start com.amazonaws.SignableRequest
 
-data {-# CLASS "com.amazonaws.http.SignableRequest" #-} SignableRequest t = SignableRequest (Object# SignableRequest t)
+data {-# CLASS "com.amazonaws.SignableRequest" #-} SignableRequest t = SignableRequest (Object# (SignableRequest t))
   deriving Class
 
 foreign import java unsafe "@interface addHeader" addHeader :: (t <: Object, a <: SignableRequest t)
@@ -103,3 +103,54 @@ foreign import java unsafe "@interface addParameter" addParameter :: (t <: Objec
 
 foreign import java unsafe "@interface setContent" setContent :: (t <: Object, a <: SignableRequest t)
   => InputStream Java a ()
+
+-- End com.amazonaws.SignableRequest
+
+-- Start com.amazonaws.Request
+
+data {-# CLASS "com.amazonaws.Request" #-} Request t = Request (Object# (Request t))
+  deriving Class
+
+foreign import java unsafe "@interface addHandlerContext"
+  addHandlerContext :: (t <: Object, x <: Object, a <: Request t) =>
+    HandlerContextKey x -> x -> Java a x
+
+foreign import java unsafe "@interface addParameters"
+  addParameters :: (t <: Object, a <: Request t) => String -> List String -> Java a ()
+
+foreign import java unsafe "@interface getAWSRequestMetrics"
+  getAWSRequestMetrics :: (t <: Object, a <: Request t) => Java a AWSRequestMetrics
+
+foreign import java unsafe "@interface addHandlerContext"
+  addHandlerContext2 :: (t <: Object, x <: Object, a <: Request t) =>
+    HandlerContextKey x -> Java a x
+
+foreign import java unsafe "@interface getOriginalRequest"
+  getOriginalRequest :: (t <: Object, a <: Request t) => Java a AWSWebServiceRequest
+
+foreign import java unsafe "@interface getServiceName"
+  getServiceName :: (t <: Object, a <: Request t) => Java a String
+
+foreign import java unsafe "@interface setAWSRequestMetrics"
+  setAWSRequestMetrics :: (t <: Object, a <: Request t) => AWSRequestMetrics -> Java a ()
+
+foreign import java unsafe "@interface setHeaders"
+  setHeaders :: (t <: Object, a <: Request t) => Map JString JString -> Java a ()
+
+foreign import java unsafe "@interface setHttpMethod"
+  setHttpMethod :: (t <: Object, a <: Request t) => HttpMethodName -> Java a ()
+
+foreign import java unsafe "@interface setParameters"
+  setParameters :: (t <: Object, a <: Request t) => Map JString (List JString) -> Java a ()
+
+foreign import java unsafe "@interface setResourcePath"
+  setResourcePath :: (t <: Object, a <: Request t) => String -> Java a ()
+
+foreign import java unsafe "@interface setTimeOffset"
+  setTimeOffset :: (t <: Object, a <: Request t) => Int -> Java a ()
+
+foreign import java unsafe "@interface withParameter"
+  withParameter :: (t <: Object, a <: Request t) => String -> String -> Java a (Request t)
+
+foreign import java unsafe "@interface withTimeOffset"
+  withTimeOffset :: (t <: Object, a <: Request t) => Int -> Java a (Request t)
