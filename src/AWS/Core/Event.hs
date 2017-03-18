17276c84b95e4e49cc21b165d6b4148303a8ed8b
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-18 23:05:23 +0530

    Added ProgressEvent to AWS.Core.Event

--- src/AWS/Core/Event.hs
+++ src/AWS/Core/Event.hs
@@ -11,3 +11,18 @@ data {-# CLASS "com.amazonaws.ProgressListener" #-} ProgressListener = ProgressL
   deriving Class
 
 foreign import java unsafe "@interface progressChanged" progressChanged :: Java ProgressListener ()
+
+-- End com.amazonaws.event.ProgressListener
+
+-- Start com.amazonaws.event.ProgressEvent
+
+data {-# CLASS "com.amazonaws.ProgressEvent" #-} ProgressEvent = ProgressEvent (Object# ProgressEvent)
+  deriving Class
+
+foreign import java unsafe getBytes :: Java ProgressEvent Int64
+
+foreign import java unsafe getBytesTransferred :: Java ProgressEvent Int64
+
+foreign import java unsafe getEventType :: Java ProgressEvent ProgressEventType
+
+-- End com.amazonaws.event.ProgressEvent
