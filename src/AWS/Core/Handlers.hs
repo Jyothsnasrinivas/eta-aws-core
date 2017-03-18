e15113cdb51e9dff995322ff491e55ae21fe85b6
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-18 23:24:37 +0530

    Started AWS.Core.Handlers

---
+++ src/AWS/Core/Handlers.hs
@@ -0,0 +1,13 @@
+module AWS.Core.Handlers where
+
+import Java
+import Java.IO
+import Java.Net
+import AWS.Core.Types
+
+-- Start com.amazonaws.handlers.HandlerContextKey
+
+data {-# CLASS "com.amazonaws.handlers.HandlerContextKey" #-} HandlerContextKey t = HandlerContextKey (Object# (HandlerContextKey t))
+  deriving Class
+
+-- End com.amazonaws.handlers.HandlerContextKey
